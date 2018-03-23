8799fd8c1f022f019f18df6facd47144a25a29a3
Comp0712 <Comp0712@PC0712>
2018-03-23 13:35:11 +0200

    Добавьте файлы проекта.

---
+++ lab1/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace lab1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form1 f1 = new Form1();
+            string s = "";
+            f1.Text = s;
+            s = textBox1.Text;
+        }
+    }
+}
